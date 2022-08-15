b6d9dce9fa349b3db0b0317d9571c289db4b3325
sofiakung5@gmail.com <sofiakung5@gmail.com>
2022-08-15 12:57:45 +0800

    fix: mobile view of intro section

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import './Intro.css'
-import profilepic from './profilePic.jpg'
-import Timeline from './Timeline'
+import React from "react";
+import "./Intro.css";
+import profilepic from "./profilePic.jpg";
+import Timeline from "./Timeline";
 const Intro = () => {
   return (
-    <div className="intro-row" id="intro">
+    <div className="intro-row">
       <div className="intro-content  intro-p1">
         <img className="profile-image" src={profilepic} alt="profile pic" />
       </div>
       <div className="intro-content intro-p2">
         <h1 className="intro-section"> &#128075; Hi I' m Sofia Kung. </h1>
-        <p className="intro-section">I am a data analyst based in Singapore.</p>
+        <p className="intro-section">
+          I fight abuse and fraud with data analytics
+        </p>
         <br></br>
         <p className="intro-section">
-          I am now taking a quarter gap-year to self-learn web development,
-          working towards being able to build a web app for my own app ideas.
+          I worked as data analyst for 3 years in consulting, product analytics
+          and now risk/fraud analytics in Gojek. During my free time I work on
+          UI/UX design, graphic design and web development
         </p>
       </div>
       <Timeline />
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
